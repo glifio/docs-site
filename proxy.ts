aa8fc575af89a62c3bfa2780d825d292d8086920
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-02 19:33:27 +0100

    Create proxy.ts

---
+++ proxy.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const locales = ['en', 'zh'] as const
+export const subdomains = ['www', 'icn'] as const
+
+const proxy = async (request: NextRequest) => {
+  // Get request data
+  const { pathname } = request.nextUrl
+  const pathParts = pathname.split('/')
+
+  // Redirect if locale is missing
+  const locale = pathParts.at(1)
+  if (locales.some(l => l === locale)) {
+    request.nextUrl.pathname = `/en${pathname}`
+    return NextResponse.redirect(request.nextUrl)
+  }
+
+  // Redirect if subdomain is missing
+  const subdomain = pathParts.at(2)
+  if (!subdomains.some(s => s === subdomain)) {
+    request.nextUrl.pathname = pathParts.toSpliced(2, 0, 'www').join('/')
+    return NextResponse.redirect(request.nextUrl)
+  }
+}
+
+export const config = {
+  matcher: [
+    // Skip files and the _next folder
+    '/((?!.*\\..*|_next).*)',
+  ],
+}
+
+export default proxy
