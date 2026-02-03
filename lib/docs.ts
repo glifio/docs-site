8a85d931ee39ae1e1fe0b69543897ba7a4d45dfa
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-03 11:38:36 +0100

    Update docs.ts

--- lib/docs.ts
+++ lib/docs.ts
@@ -1,4 +1,8 @@
 import 'server-only'
+import * as fs from 'node:fs/promises'
+import * as path from 'node:path'
+
+const DOCS_DIR = path.join(process.cwd(), 'docs')
 
 interface Doc {
   title: string
@@ -8,7 +12,7 @@ interface Doc {
 
 interface DocParams {
   locale: string
-  domain: string
+  subdomain: string
   slug: string[]
   url: string
 }
