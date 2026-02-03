667d48fae1276b0b1a1a5f68e469ff63319dff01
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-03 15:49:43 +0100

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ locale, subdomain }: SidebarProps) => {
   return (
-    <div className='flex flex-col gap-6'>
+    <div className='flex flex-col gap-6 prose prose-gray'>
       <h2>Docs</h2>
     </div>
   )
