07cf483c7e8f01c9c0e6688b47d9d3b182cc0f63
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-03 16:01:01 +0100

    Update layout.tsx

--- app/[locale]/[subdomain]/layout.tsx
+++ app/[locale]/[subdomain]/layout.tsx
@@ -2,8 +2,8 @@ import './style.css'
 
 import { ReactNode } from 'react'
 import type { Metadata } from 'next'
-
 import { Sidebar } from '@/components/Sidebar'
+import { getDocsTree } from '@/lib/docs'
 
 interface LayoutProps {
   children: ReactNode
@@ -12,6 +12,7 @@ interface LayoutProps {
 
 const Layout = async ({ children, params }: LayoutProps) => {
   const { locale, subdomain } = await params
+  const tree = await getDocsTree(locale, subdomain)
 
   return (
     <html lang={locale}>
@@ -22,7 +23,7 @@ const Layout = async ({ children, params }: LayoutProps) => {
           <div className='flex'>
             {/* Sidebar */}
             <div className='flex-none sticky top-0 w-xs p-8'>
-              <Sidebar locale={locale} subdomain={subdomain} />
+              <Sidebar locale={locale} subdomain={subdomain} tree={tree} />
             </div>
             {/* Page */}
             <div className='grow w-full max-w-6xl mx-auto p-8'>{children}</div>
