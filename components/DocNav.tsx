3296e2b0817c602f17f7e6be82c2e4b6fd899cd8
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-04 16:01:30 +0100

    Update DocNav.tsx

--- components/DocNav.tsx
+++ components/DocNav.tsx
@@ -7,28 +7,29 @@ interface DocNavProps {
   tree: DocTree
   title?: string
   small?: boolean
+  collapse?: boolean
 }
 
-export const DocNav = ({ tree, title, small }: DocNavProps) => (
+export const DocNav = ({ tree, title, small, collapse }: DocNavProps) => (
   <nav className={`prose prose-gray ${small ? 'prose-sm' : ''}`}>
     <h2>{title ?? <Link href={tree.url}>{tree.title}</Link>}</h2>
 
     <ul className='pl-0 list-none'>
       {tree.children.map(child => (
-        <DocLink key={child.url} node={child} />
+        <DocNode key={child.url} node={child} />
       ))}
     </ul>
   </nav>
 )
 
-const DocLink = ({ node }: { node: DocTree | DocLeaf }) => (
+const DocNode = ({ node }: { node: DocTree | DocLeaf }) => (
   <li>
     <Link href={node.url}>{node.title}</Link>
 
     {'children' in node && (
       <ul className='ml-1.5 pl-1.5 border-l border-current/25 list-none'>
         {node.children.map(child => (
-          <DocLink key={child.url} node={child} />
+          <DocNode key={child.url} node={child} />
         ))}
       </ul>
     )}
