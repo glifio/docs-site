e720edae4e080fa3f6aa8fe895fc93c1286e82d5
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-04 14:28:07 +0100

    Update DocNav.tsx

--- components/DocNav.tsx
+++ components/DocNav.tsx
@@ -1,3 +1,24 @@
 'use client'
 
 import Link from 'next/link'
+import { DocTree, DocLeaf } from '@/lib/docs'
+
+interface DocNavProps {
+  tree: DocTree
+  showTitle?: boolean
+}
+
+export const DocNav = ({ tree, showTitle }: DocNavProps) => (
+  <nav className='prose prose-gray prose-sm'>
+    {showTitle && (
+      <h2>
+        <Link href={tree.url}>{tree.title}</Link>
+      </h2>
+    )}
+    <ul className='pl-0 list-none'>
+      {tree.children.map(child => (
+        <DocTreeNode key={child.url} node={child} />
+      ))}
+    </ul>
+  </nav>
+)
