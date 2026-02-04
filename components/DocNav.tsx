bbd31f09bbda062408724bfe6659eb2e466135c8
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-04 14:31:11 +0100

    Update DocNav.tsx

--- components/DocNav.tsx
+++ components/DocNav.tsx
@@ -15,10 +15,25 @@ export const DocNav = ({ tree, showTitle }: DocNavProps) => (
         <Link href={tree.url}>{tree.title}</Link>
       </h2>
     )}
+
     <ul className='pl-0 list-none'>
       {tree.children.map(child => (
-        <DocTreeNode key={child.url} node={child} />
+        <DocLink key={child.url} node={child} />
       ))}
     </ul>
   </nav>
+)
+
+const DocLink = ({ node }: { node: DocTree | DocLeaf }) => (
+  <li>
+    <Link href={node.url}>{node.title}</Link>
+
+    {'children' in node && (
+      <ul className='ml-2 pl-2 border-l border-current/25 list-none'>
+        {node.children.map(child => (
+          <DocLink key={child.url} node={child} />
+        ))}
+      </ul>
+    )}
+  </li>
 )
