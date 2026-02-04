f885812109436f808178b328bc1c647b68a60021
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-04 14:20:34 +0100

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { DocNav } from '@/components/DocNav'
 import { DocTree } from '@/lib/docs'
+import { locales, subdomains } from '@/lib/env'
 
 interface SidebarProps {
   locale: string
@@ -9,7 +11,8 @@ interface SidebarProps {
   tree: DocTree | null
 }
 
-export const Sidebar = ({ tree }: SidebarProps) => {
+export const Sidebar = ({ locale, subdomain, tree }: SidebarProps) => {
+  const router = useRouter()
   return (
     <nav className='prose prose-gray prose-sm'>
       {tree && <DocNav tree={tree} title />}
