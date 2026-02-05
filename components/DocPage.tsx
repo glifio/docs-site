a407dedcb49fb7d36ab49383eae3ff4ccd62d3ca
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-05 13:30:59 +0100

    Update DocPage.tsx

--- components/DocPage.tsx
+++ components/DocPage.tsx
@@ -8,26 +8,35 @@ import { DocNav } from '@/components/DocNav'
 import { DocTree } from '@/lib/docs'
 
 interface DocPageProps {
-  children: string
+  content: string
+  footer: string | null
   tree: DocTree | null
 }
 
-export const DocPage = ({ children, tree }: DocPageProps) => (
+export const DocPage = ({ content, footer, tree }: DocPageProps) => (
   <article className='prose prose-gray max-w-none'>
-    <ReactMarkdown
-      remarkPlugins={[remarkGfm, remarkMath, remarkAlert]}
-      rehypePlugins={[rehypeKatex]}
-      components={{
-        img: ({ src, alt }) => {
-          if (!src || typeof src !== 'string') return null
-          const imgSrc = /^(https?:\/\/|\/)/.test(src) ? src : `/${src}`
-          return <img src={imgSrc} alt={alt} /> // eslint-disable-line @next/next/no-img-element
-        },
-      }}
-    >
-      {children}
-    </ReactMarkdown>
-
+    <DocMarkdown content={content} />
     {tree && <DocNav tree={tree} title='Table of Contents' />}
+    {footer && <DocMarkdown content={content} />}
   </article>
+)
+
+interface DocMarkdownProps {
+  content: string
+}
+
+const DocMarkdown = ({ content }: DocMarkdownProps) => (
+  <ReactMarkdown
+    remarkPlugins={[remarkGfm, remarkMath, remarkAlert]}
+    rehypePlugins={[rehypeKatex]}
+    components={{
+      img: ({ src, alt }) => {
+        if (!src || typeof src !== 'string') return null
+        const imgSrc = /^(https?:\/\/|\/)/.test(src) ? src : `/${src}`
+        return <img src={imgSrc} alt={alt} /> // eslint-disable-line @next/next/no-img-element
+      },
+    }}
+  >
+    {content}
+  </ReactMarkdown>
 )
