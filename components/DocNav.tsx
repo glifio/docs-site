b003f41ada3be04fe3a794532ab2ddb8634ab479
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-05 07:55:32 +0100

    Update DocNav.tsx

--- components/DocNav.tsx
+++ components/DocNav.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { DocTree, DocLeaf } from '@/lib/docs'
 
 interface DocNavProps {
@@ -10,18 +11,26 @@ interface DocNavProps {
   collapse?: boolean
 }
 
-export const DocNav = ({ tree, title, small, collapse }: DocNavProps) => (
-  <nav className={`prose prose-gray ${small ? 'prose-sm' : ''}`}>
-    <h2>{title ?? <Link href={tree.url}>{tree.title}</Link>}</h2>
+export const DocNav = ({ tree, title, small, collapse }: DocNavProps) => {
+  const pathname = usePathname()
 
-    <ul className='pl-0 list-none'>
-      {tree.children.map(child => (
-        <DocNode key={child.url} node={child} />
-      ))}
-    </ul>
-  </nav>
-)
+  return (
+    <nav className={`prose prose-gray ${small ? 'prose-sm' : ''}`}>
+      <h2>{title ?? <Link href={tree.url}>{tree.title}</Link>}</h2>
 
+      <ul className='pl-0 list-none'>
+        {tree.children.map(child => (
+          <DocNode
+            key={child.url}
+            node={child}
+            collapse={collapse}
+            pathname={pathname}
+          />
+        ))}
+      </ul>
+    </nav>
+  )
+}
 const DocNode = ({ node }: { node: DocTree | DocLeaf }) => (
   <li>
     <Link href={node.url}>{node.title}</Link>
