e418f5da63433e643617a9ed1c728fc6ac17d8ba
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-24 09:44:40 +0100

    Delete env.ts

--- lib/env.ts
+++
@@ -1,7 +0,0 @@
-export const locales = ['en', 'zh'] as const
-export const subdomains = ['www', 'icn'] as const
-
-export type Locale = (typeof locales)[number]
-
-export const isLocale = (locale: string): locale is Locale =>
-  locales.some(l => l === locale)
