d66d64a0dd7c5e3027f7e97edf1e87de273b0bea
Thijs van Hoof <thijs.v.h@gmail.com>
2026-02-25 15:33:34 +0100

    Update utils.ts

--- components/utils.ts
+++ components/utils.ts
@@ -2,10 +2,10 @@ export const getDocUrl = (
   locale: string,
   subdomain: string,
   route: string,
-  isSubdomainHost?: boolean,
+  isSubdomainHost: boolean,
 ) => `/${locale}${isSubdomainHost ? '' : `/${subdomain}`}${route}`
 
-export const getFileUrl = (path: string, publicRoot?: `/${string}`) =>
-  `${publicRoot ?? ''}/${path}`
+export const getFileUrl = (path: string, publicRoot: `/${string}`) =>
+  `${publicRoot}${publicRoot.endsWith('/') ? '' : '/'}${path}`
 
 export const isExtUrl = (url: string) => /^https?:\/\//.test(url)
