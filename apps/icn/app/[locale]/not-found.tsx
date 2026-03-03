cf7aad0df9c8abc281b99bdaff395d9e82ef3f94
Thijs van Hoof <thijs.v.h@gmail.com>
2026-03-03 11:59:34 +0100

    404

--- apps/icn/app/[locale]/not-found.tsx
+++ apps/icn/app/[locale]/not-found.tsx
@@ -1,14 +1,5 @@
 import { NotFound } from '@glif/shared/components'
-import { isLocale } from '@glif/shared/types'
 
-interface PageProps {
-  params: Promise<{ locale: string }>
-}
+const NotFoundPage = () => <NotFound />
 
-const Page = async ({ params }: PageProps) => {
-  const { locale } = await params
-  if (!isLocale(locale)) throw new Error(`Invalid locale: ${locale}`)
-  return <NotFound locale={locale} />
-}
-
-export default Page
+export default NotFoundPage
