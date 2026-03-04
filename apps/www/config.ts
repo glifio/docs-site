6053b77cac401e352d86bc946ffc9324839efd03
Thijs van Hoof <thijs.v.h@gmail.com>
2026-03-04 14:42:18 +0100

    name

--- apps/www/config.ts
+++ apps/www/config.ts
@@ -1,6 +1,7 @@
 import path from 'node:path'
 import type { Locale } from '@glif/shared/types'
 
+export const siteName = 'GLIF Docs'
 export const siteUrl = 'https://docs.glif.io'
 export const docsDir = path.join(process.cwd(), 'docs')
 export const locales: Locale[] = ['en', 'zh']
