21956cf26ca9466034fbd6771b658e147975fa91
Thijs van Hoof <thijs.v.h@gmail.com>
2026-03-04 14:29:11 +0100

    Update next.config.ts

--- apps/icn/next.config.ts
+++ apps/icn/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
 
+import { nextConfigHeaders } from '@glif/shared/utils-server'
+
 const nextConfig: NextConfig = {
   transpilePackages: ['@glif/shared'],
+  headers: nextConfigHeaders,
 }
 
 export default nextConfig
